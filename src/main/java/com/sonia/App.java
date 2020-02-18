ad020da748f84dac34597f017d4abbf792e81c6b
WilliamHenryKing <kingiamwill@gmail.com>
2020-02-18 21:32:37 +0200

    Added a line of printing code.

--- src/main/java/com/sonia/App.java
+++ src/main/java/com/sonia/App.java
@@ -5,6 +5,9 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Assignment 1" );
+
+        System.out.println("This line was added by William King.");
     }
 }
